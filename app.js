f9c88cf1e96717fca4fba0cc7a7789ae8cf7344f
Jordan Smith <cjordansmith@gmail.com>
2016-05-14 06:09:24 -0500

    from windows

--- app.js
+++ app.js
@@ -5,19 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// tells Express where to find 'routes' folder
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+// var users = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-// tells Express where to find 'views' folder
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
