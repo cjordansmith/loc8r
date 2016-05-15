8106232c9cddc4d4df8773149b2d2a929073d1c9
jordan <cjordansmith@gmail.com>
2016-05-15 05:03:41 -0500

    creating controllers for app_api

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+// Point the model to the correct place
+require('./app_api/models/db');
 
+// tells application where to look for routes folders
 var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 // var users = require('./app_server/routes/users');
 
 var app = express();
@@ -23,7 +26,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// tells application when to use the routes
 app.use('/', routes);
+app.use('/api', routesApi);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ app_api/controllers/locations.js
@@ -0,0 +1,223 @@
+// Require Mongoose into /app_api/locations.js
+// Give the controller access to the database connection
+var mongoose = require('mongoose');
+// Bring in the Location model to interact with the Locations collection
+var Loc = mongoose.model('Location');
+
+// Return JSON and response status
+var sendJSONresponse = function(res, status, content) {
+    res.status(status);
+    res.json(content);
+};
+
+var theEarth = (function() {
+    // define fixed value for radius of Earth
+    var earthRadius = 3959; // 6371 km, 3959 mi
+
+    // convert radians to distance
+    var getDistanceFromRads = function(rads) {
+        return parseFloat(rads * earthRadius);
+    };
+
+    // convert distance to radians
+    var getRadsFromDistance = function(distance) {
+        return parseFloat(distance / earthRadius);
+    };
+
+    // expose these two functions
+    return {
+        getDistanceFromRads : getDistanceFromRads,
+        getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
+var buildLocationList = function(req, res, results, stats) {
+    var locations = [];
+    results.forEach(function(doc) {
+        locations.push({
+            distance: theEarth.getDistanceFromRads(doc.dis),
+            name: doc.obj.name,
+            address: doc.obj.address,
+            rating: doc.obj.rating,
+            facilities: doc.obj.facilities,
+            _id: doc.obj._id
+        });
+    });
+    return locations;
+};
+
+// Controller for listing search locations GET
+module.exports.locationsListByDistance = function (req, res) {
+    var lng = parseFloat(req.query.lng);
+    var lat = parseFloat(req.query.lat);
+    var point = {
+        type: "Point",
+        coordinates: [lng, lat]
+    };
+    var geoOptions = {
+        spherical: true,
+        maxDistance: theEarth.getRadsFromDistance(20),
+        num: 10 // limit the size of the returned data set
+    };
+    if (!lng || !lat) {
+        console.log('locationsListByDistance missing params');
+        sendJSONresponse(res, 404, {
+            "message": "lng and lat query parameters are required"
+        });
+        return;
+    }
+    Loc.geoNear(point, geoOptions, function(err, results, stats) {
+        var locations;
+        console.log('GEO Results', results);
+        console.log('GEO stats', stats);
+        if (err) {
+            console.log('geoNear error:', err);
+            sendJSONresponse(res, 404, err);
+        } else {
+            results.forEach(function(doc) {
+                locations.push({
+                    distance: theEarth.getDistanceFromRads(doc.dis),
+                    name: doc.obj.name,
+                    address: doc.obj.address,
+                    rating: doc.obj.rating,
+                    facilities: doc.obj.facilities,
+                    _id: doc.obj._id
+                });
+            });
+            sendJSONresponse(res, 200, locations);
+        }
+    });
+};
+
+// Controller for creating a location POST
+// /api/locations
+module.exports.locationsCreate = function (req, res) {
+    console.log(req.body);
+    Loc.create({
+        name: req.body.name,
+        address: req.body.address,
+        facilities: req.body.facilities.split(","),
+        coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+        openingTimes: [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1,
+        }, {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2,
+        }]
+    }, function(err, location) {
+        if (err) {
+            console.log(err);
+            sendJSONresponse(res, 400, err);
+        } else {
+            console.log(location);
+            sendJSONresponse(res, 201, location);
+        }
+    });
+};
+
+// Controller for reading a location by the id GET
+module.exports.locationsReadOne = function (req, res) {
+    console.log('Finding location details', req.params);
+    if (req.params && req.params.locationid) {// check that locationid exists in request parameters
+        Loc
+            .findById(req.params.locationid)
+            .exec(function(err, location) {
+                if (!location) {// if Mongoose doesn't return a location, send 404 message and exit function scope using return statement
+                    sendJSONresponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {// if Mongoose returned an error, send it as 404 response and exit controller using return statement
+                    console.log(err);
+                    sendJSONresponse(res, 404, err);
+                    return;
+                }
+                console.log(location);
+                sendJSONresponse(res, 200, location);
+            });
+    } else {// if request parameters didn't include locationid, send appropriate 404 response
+        console.log('No locationid specified');
+        sendJSONresponse(res, 404, {
+            "message": "No locationid in request"
+        });
+    }
+};
+
+// Controller for updating a location PUT
+// PUT /api/locations/:locationid
+module.exports.locationsUpdateOne = function(req, res) {
+    if (!req.params.locationid) {
+        sendJSONresponse(res, 404, {
+            "message": "Not found, locationid is required"
+        });
+        return;
+    }
+    Loc
+        .findById(req.params.locationid)
+        .select('-reviews -rating')
+        .exec(
+            function(err, location) {
+                if (!location) {
+                    sendJSONresponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJSONresponse(res, 400, err);
+                    return;
+                }
+                location.name = req.body.name;
+                location.address = req.body.address;
+                location.facilities = req.body.facilities.split(",");
+                location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+                location.openingTimes = [{
+                    days: req.body.days1,
+                    opening: req.body.opening1,
+                    closing: req.body.closing1,
+                    closed: req.body.closed1,
+                }, {
+                    days: req.body.days2,
+                    opening: req.body.opening2,
+                    closing: req.body.closing2,
+                    closed: req.body.closed2,
+                }];
+                location.save(function(err, location) {
+                    if (err) {
+                        sendJSONresponse(res, 404, err);
+                    } else {
+                        sendJSONresponse(res, 200, location);
+                    }
+                });
+            }
+    );
+};
+
+// Controller for deleting a location
+// DELETE /api/locations/:locationid
+module.exports.locationsDeleteOne = function(req, res) {
+    var locationid = req.params.locationid;
+    if (locationid) {
+        Loc
+            .findByIdAndRemove(locationid)
+            .exec(
+                function(err, location) {
+                    if (err) {
+                        console.log(err);
+                        sendJSONresponse(res, 404, err);
+                        return;
+                    }
+                    console.log("Location id " + locationid + " deleted");
+                    sendJSONresponse(res, 204, null);
+                }
+        );
+    } else {
+        sendJSONresponse(res, 404, {
+            "message": "No locationid"
+        });
+    }
+};
